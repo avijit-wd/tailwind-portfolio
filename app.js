4c6b54ca87994cfe0dd5822fbb1d8de2030c47e4
Avijit Biswas <avijitb440@gmail.com>
2022-08-22 23:22:05 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector('#hamburger');
+const menu = document.querySelector('#menu');
+const hLinks = document.querySelectorAll('#hLink');
+const body = document.querySelector('body');
+const moonBtn = document.querySelector('#moon');
+
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
+
+hLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
+});
+
+moon.addEventListener('click', () => {
+  body.classList.toggle('dark');
+});
